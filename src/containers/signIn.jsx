a82d79b8938e37e698dda7187dc1b4bdfb524f63
zach <zachwinters1@gmail.com>
2023-03-06 18:31:06 -0800

    sign in/sign up

---
+++ src/containers/signIn.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+const SignIn = () => {
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="sign-in">
+
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ src/containers/signUp.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+const SignUp = () => {
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div className="sign-up">
+
+        </div>
+    );
+}
+
+export default SignUp;

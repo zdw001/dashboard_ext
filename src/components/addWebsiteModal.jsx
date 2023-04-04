859896f7a554e1bba2838ba734a156d269e03aaf
zach <zachwinters1@gmail.com>
2023-04-03 20:47:02 -0700

    text area

--- src/components/addWebsiteModal.jsx
+++ src/components/addWebsiteModal.jsx
@@ -8,6 +8,7 @@ import Modal from './modal';
 const AddWebsiteModal = (props) => {
     const [websiteName, setWebsiteName] = useState("");
     const [websiteLink, setWebsiteLink] = useState("");
+    const [websiteNotes, setWebsiteNotes] = useState("");
 
     const generateRandomLogo = (str) => {
         let hash = 0;
@@ -44,11 +45,9 @@ const AddWebsiteModal = (props) => {
                     <input value={websiteLink} onChange={e => setWebsiteLink(e.target.value)}/>
                     <label>Website Link</label>
                 </div>
-                <div className='half-width'>
-                    <div className='input-wrapper'>
-                        <input value={websiteLink} onChange={e => setWebsiteLink(e.target.value)}/>
-                        <label>Website Link</label>
-                    </div>
+                <div className='input-wrapper'>
+                    <textarea value={websiteNotes} onChange={e => setWebsiteNotes(e.target.value)}/>
+                    <label>Notes</label>
                 </div>
             </div>
         </Modal>
